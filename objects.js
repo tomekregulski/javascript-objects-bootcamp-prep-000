96fbeceb7fab2be905f48881ef6ee485a80eff92
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-11 23:52:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 
-// function removeFromPlaylist(playlist, artistName) {
-//   delete playlist[artistName];
-//   return playlist;
-// }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
