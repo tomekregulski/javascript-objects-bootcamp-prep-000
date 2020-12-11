a87ff01e4fdf245da5c20c3b4766bbe3a79e69f3
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-11 23:46:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,12 +2,12 @@ var playlist = {}
 var playlist = new Object();
 var playlist = {dagar: "yaman"};
 
-// function updatePlayist(obj, artistName, songTitle) {
-//   Object.assign({}, obj, { [artisName]: songTitle });
-//   return playlist
-// }
+// // function updatePlayist(obj, artistName, songTitle) {
+// //   Object.assign({}, obj, { [artisName]: songTitle });
+// //   return playlist
+// // }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName];
-  return playlist;
-}
+// function removeFromPlaylist(playlist, artistName) {
+//   delete playlist[artistName];
+//   return playlist;
+// }
