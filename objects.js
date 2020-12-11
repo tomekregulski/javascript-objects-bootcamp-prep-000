a0cd1ef1bcce01973c2ca875a624bed524a39d54
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-11 23:40:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {}
 var playlist = new Object();
-var playlist = {dagar: "yaman"}
+var playlist = {dagar: "yaman"};
 
 // function updatePlayist(obj, artistName, songTitle) {
 //   Object.assign({}, obj, { [artisName]: songTitle });
